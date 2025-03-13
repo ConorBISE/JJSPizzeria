939a9a742299d2b0ceee4bc7dc519d13f130ac6f
alisiak04 <alisiakazimierek2104@gmail.com>
2025-03-13 20:36:30 +0000

    fixed inline

--- src/main/java/org/jjspizzeria/jjspizzeria/customer/HappyDialogue.java
+++ src/main/java/org/jjspizzeria/jjspizzeria/customer/HappyDialogue.java
@@ -3,16 +3,16 @@ package org.jjspizzeria.jjspizzeria.customer;
 public class HappyDialogue implements Personality {
     @Override
     public String greetingDialogue(Customer customer) {
-        return "\nHi there! I'm " + customer.getName() + ". I would love to order " + customer.getOrder().getOrderDetails() + "!";
+        return "Hi there! I'm " + customer.getName() + ". I would love to order " + customer.getOrder().getOrderDetails() + "!";
     }
 
     @Override
     public String ratingDialogue(Customer customer) {
-        return "\nWow, this pizza looks amazing! I can't wait to dig in!";
+        return "Wow, this pizza looks amazing! I can't wait to dig in!";
     }
 
     @Override
     public String leavingDialogue(Customer customer) {
-        return "\nThanks so much for the pizza! Have a great day!! Goodbye :) ";
+        return "Thanks so much for the pizza! Have a great day!! Goodbye :) ";
     }
 }
